2b976a2a22d507c684cbe6e86ed042bc45428399
Rob <rdejournett@gmail.com>
2026-02-10 09:14:59 -0500

    more aggressive

--- common/defines/naval_ai.lua
+++ common/defines/naval_ai.lua
@@ -9,3 +9,7 @@ NDefines.NAI.MISSING_CONVOY_ESCORT_IMPORTANCE_FACTOR = 2.0
 -- In common/defines/naval_ai.lua
 NDefines.NAI.NAVAL_TRANSFER_PRIORITY = 2.0 -- AI moves ships to active fleets faster
 NDefines.NAI.MAX_REASONABLE_REPAIR_PRIORITY = 0.05 -- Prevents fleets from sitting in port for 1% damage
+-- Tell the AI to be much more aggressive with how many groups it forms
+NDefines.NAI.MAX_TASKFORCE_COUNT_FOR_REGION_ADDITIONAL_PRIORITY = 20
+NDefines.NAI.MIN_REASONABLE_TASKFORCE_SIZE = 1 -- AI is allowed to make 1-ship groups
+-- Lowering this makes the AI "deathstack" its capitals even if it's short on destroyers
